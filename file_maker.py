be4d698875688ffc3111c85c7a688cf7dbc343e7
w7Pratham <prathamshingne@gmail.com>
2023-07-04 21:04:05 +0530

    First Commit

---
+++ file_maker.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+list_of_files = [
+    "main/__init__.py",
+    "main/routes.py",
+    "posts/__init__.py",
+    "posts/routes.py",
+    "posts/forms.py",
+    "users/__init__.py",
+    "users/routes.py",
+    "users/forms.py",
+    "users/utils.py",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
---
+++ flaskblog/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'aethergen36@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+mail = Mail(app)
+
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)

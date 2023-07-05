aea74a9511f6bcfc3e59bbbec734fef67469f0e5
w7Pratham <prathamshingne@gmail.com>
2023-07-05 15:01:47 +0530

    config changes

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,6 +1,6 @@
 
 class Config:
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SECRET_KEY = ''
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
